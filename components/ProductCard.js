73ef0bf5203f0f7d2bc34756e60689e58a0ed9bc
Jonathan Huergo <jh2000@hotmail.com.ar>
2023-03-06 14:40:41 -0300

    Style ProductCard

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -4,18 +4,20 @@ import styles from '../styles/ProductCard.module.css'
 const ProductCard = ({ product }) => {
     return (
         <Link href={`./product/${product.slug}`} key={product.id}>
-          <div className={styles.emblaSlide}>
-            <div className={styles.emblaSlideImgContainer}>
-              <img src={product.mainPhoto.url} alt={product.name} className={styles.emblaSlideImg}/>
+          <div className={styles.productCard}>
+            <div className={styles.productCardImgContainer}>
+              <img src={product.mainPhoto.url} alt={product.name} className={styles.productCardImg}/>
             </div>
-            <h3>{product.name}</h3>
-            {product.isOnSale?
-                <div>
-                    <p className={styles.salePrice}><b>$ {product.salePrice}</b></p>
-                    <p className={styles.price}><s>$ {product.price}</s></p>
-                </div>
-                :
-                <p className={styles.price}><b>$ {product.price}</b></p>}
+            <div className={styles.productCardTextContainer}>
+              <h3>{product.name}</h3>
+              {product.isOnSale?
+                  <div>
+                      <p className={styles.salePrice}><b>$ {product.salePrice}</b></p>
+                      <p className={styles.price}><s>$ {product.price}</s></p>
+                  </div>
+                  :
+                  <p className={styles.price}><b>$ {product.price}</b></p>}
+            </div>
           </div>
         </Link>
     );
