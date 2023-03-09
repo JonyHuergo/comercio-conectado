944d672873d0cd6f255a9b70714da226663392e3
Jonathan Huergo <jh2000@hotmail.com.ar>
2023-03-09 19:08:47 -0300

    Add export for CarouselCard component

--- components/index.js
+++ components/index.js
@@ -4,3 +4,5 @@ export { default as FeaturedProducts } from './FeaturedProducts';
 export { default as ProductsOnSale } from './ProductsOnSale';
 export { default as FeaturedBrands } from './FeaturedBrands';
 export { default as Footer } from './Footer';
+export { default as CarouselCard } from './CarouselCard';
+export { default as ProductCard } from './ProductCard';
