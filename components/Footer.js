07cdcb93c2bb26d5fd61ab32e42fcd9f5ab96c05
Jonathan Huergo <jh2000@hotmail.com.ar>
2023-03-12 15:24:30 -0300

    Remove unused elements from Footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,12 +3,7 @@ import styles from '../styles/Footer.module.css'
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <div className={styles.footerContainer}>
-                <h4 className={styles.footerItem}>Trabaja con nosotros</h4>
-                <h4 className={styles.footerItem}>Terminos y condiciones</h4>
-                <h4 className={styles.footerItem}>Ayuda</h4>
-            </div>
-            <small className={styles.copyright}>Copyright &copy; Jonathan Huergo</small>
+            <p className={styles.copyright}>Copyright &copy; Jonathan Huergo</p>
         </footer>
     );
 }
